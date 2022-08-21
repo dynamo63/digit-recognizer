1c392c571dec3a550f00955c3f7b88e070c387dd
dynamo63 <smoulongui@gmail.com>
2022-08-21 14:15:01 +0100

    ready to deploy

---
+++ README.md
@@ -0,0 +1,4 @@
+# Digit Recognition Demo App
+
+I create my first CNN model with TensorFlow, the goal is to predict a handwritten or drawn digit.
+you can find the source code of the model [here](https://www.kaggle.com/code/mouckeytoumoulongui/get-started-cnn)
--- app.py
+++ app.py
@@ -1,24 +1,33 @@
 import streamlit as st
+st.set_page_config(page_title="Digit Recognition")
 import tensorflow as tf
 from streamlit_drawable_canvas import st_canvas
-from utils import pil_resize_image
 import numpy as np
 import cv2
 
 # Load model
-model = tf.keras.models.load_model("./digit_recognizer.h5")
+model = tf.keras.models.load_model("./models/digit_recognizer.h5")
 
 st.title("Hey, Welcome to my digit recognition app")
+st.markdown("""
+    [Here](https://www.kaggle.com/code/mouckeytoumoulongui/get-started-cnn) the Kaggle link for the model.
+    Interest to code ? Here my [repo](https://github.com/dynamo63/digit-recognizer)
+""")
 
-st.write("Type a digit")
+col1, col2 = st.columns(2)
 
-canvas_board = st_canvas(width=300, height=300, stroke_color='red')
+with col1:
+    st.write("Type a digit")
+    canvas_board = st_canvas(width=300, height=300, stroke_color='red')
+    is_clicked = st.button("predict")
 
-if canvas_board.image_data is not None:
-    img = canvas_board.image_data
-    img_resized = cv2.resize(img.astype('uint8'), (28, 28))
-    rescaled = cv2.resize(img_resized, (300, 300), interpolation=cv2.INTER_NEAREST)
-    img_gray = cv2.cvtColor(img_resized, cv2.COLOR_BGR2GRAY)
-    pred = model.predict(tf.reshape(img_gray, [1, 28, 28, 1]))
-    st.image(rescaled)
-    st.subheader(f"I'm try to predict, the digit is {np.argmax(pred)}, right ?")
+
+with col2:
+    if canvas_board.image_data is not None and is_clicked:
+        img = canvas_board.image_data
+        img_resized = cv2.resize(img.astype('uint8'), (28, 28))
+        rescaled = cv2.resize(img_resized, (300, 300), interpolation=cv2.INTER_NEAREST)
+        img_gray = cv2.cvtColor(img_resized, cv2.COLOR_BGR2GRAY)
+        pred = model.predict(tf.reshape(img_gray, [1, 28, 28, 1]))
+        st.image(rescaled)
+        st.subheader(f"I'm try to predict, the digit is {np.argmax(pred)}, right ?")
